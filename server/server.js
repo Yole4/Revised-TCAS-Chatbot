2514fbde430a0aca7f6becc505a9539770c930a1
Eloy <shelomora60@gmail.com>
2024-01-13 18:05:51 +0800

    sdfsdf

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-// const nodemailer = require('nodemailer');
 
 // port number
 const port = process.env.DB_PORT;
@@ -28,29 +27,6 @@ app.use('/api/chatbot', chatbotRoutes);
 app.get('/', (req, res) => {
     res.send("Welcome to TCAS Backend");
 });
-
-// var transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: 'shelomora60@gmail.com',
-//         pass: 'clvqembmjluvefqr'
-//     }
-// });
-
-// var mailOptions = {
-//     from: 'shelomora60@gmail.com',
-//     to: "shelomora13@gmail.com",
-//     subject: 'Your verification code!',
-//     text: "Testing"
-// };
-
-// transporter.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//         console.log(error);
-//     } else {
-//         console.log("sent success!");
-//     }
-// });
 
 app.listen(port, (req, res) => {
     console.log(`Server is running on port ${port}`);
